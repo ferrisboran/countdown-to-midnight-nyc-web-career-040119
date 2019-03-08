d82bef74bd643e4e5732a3eaa99021b37f6e179b
Ferris Boran <ferris.boran@gmail.com>
2019-03-08 16:53:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,11 @@
 #write your code here
 
 def countdown(int)
+  int = 10
   while int >= 0
-    case int
-    when int = 1
-      puts "#{int} SECOND!"
-      sleep(1)
-    end
-    puts "#{int} SECONDS!"
+    puts "#{int} SECOND(S)!"
     sleep(1)
+    int = int - 1
   end
+puts "HAPPY NEW YEAR!"
 end
